dce12033857815c3d6d603a76fb88af90e92433f
unknown <suyashgaikwad31@gmail.com>
2021-03-15 11:00:27 +0530

    added TicTac.java

--- TicTac.java
+++ TicTac.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class TicTac
+public class TicTacToeGame
 {
     public static void main(String[] args)
     {
@@ -18,21 +18,36 @@ public class TicTac
 
         }
         System.out.println("Comput Letter is: " + ComputLetter+" User Letter is : " + UserLetter);
+        showBoard(board);
     }
-    public static char chooseLetter(){
+    public static char chooseLetter()
+    {
         char symbol;
         Scanner UserInput = new Scanner(System.in);
         System.out.print("Enter your symbol");
-        symbol=UserInput.next().toUpperCase().charAt(0);
+        symbol = UserInput.next().toUpperCase().charAt(0);
         return symbol;
+    }
+
+    public static void showBoard(char[] board)
+    {
+        System.out.println(" ");
+        System.out.println("\n "+ board[1] + " | "+ board[2] + " | " + board[3]);
+        System.out.println("-----------");
+        System.out.println(" "+ board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("-----------");
+        System.out.println(" "+ board[7] + " | " + board[8] + " | " + board[9]);
+        System.out.println(" ");
+
     }
     public static char[] createBoard()
     {
         char[] board = new char[10];
-        for(int i = 0; i < board.length; i++)
+        for(int i = 1; i < board.length; i++)
         {
             board[i] = ' ';
         }
         return  board;
     }
 }
+
